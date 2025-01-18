1b3432bd042e8f5615a16e85bdd1b9e1c78c2605
1F10230171 <s1f102301718@iniad.org>
2025-01-18 23:22:31 +0900

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  const containerStyle={
+    display:'flex',
+    justifyContent:'center'
+  }
+  return(
+    <div className='App'>
+    <h2>Minesweeper</h2>
+    <div style={containerStyle}>
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
